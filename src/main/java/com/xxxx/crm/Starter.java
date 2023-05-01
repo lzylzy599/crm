7cba5fec586288519c8c4c37033db2e43384fab5
Gunther <2161996735@qq.com>
2023-05-01 16:17:24 +0800

    crm第一次提交

---
+++ src/main/java/com/xxxx/crm/Starter.java
@@ -0,0 +1,35 @@
+package com.xxxx.crm;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 乐字节：专注线上IT培训
+ * 答疑老师微信：lezijie
+ */
+@SpringBootApplication
+@MapperScan("com.xxxx.crm.dao")
+@EnableScheduling // 启用定时任务
+public class Starter extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class);
+        try {
+            Runtime.getRuntime().exec("cmd   /c   start   http://127.0.0.1:8080/crm/index");//可以指定自己的路径
+        } catch (Exception ex) {
+            ex.printStackTrace(
+            );
+        }
+    }
+
+    /* 设置Web项目的启动入口 */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Starter.class);
+
+    }
+}
